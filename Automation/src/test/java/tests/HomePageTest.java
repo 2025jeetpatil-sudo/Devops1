7f205a2ff80c5a0bf490965dba3e69e3fbe0ec0e
Jeet Patil <2025.jeet.patil@ves.ac.in>
2026-03-01 21:49:11 +0530

    Added Selenium TestNG Automation project

---
+++ Automation/src/test/java/tests/HomePageTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomePageTest {
+
+    @Test
+    public void testHomePageLoads() {
+        WebDriver driver = getDriver();
+        driver.get("http://localhost:5173");
+        Assert.assertNotNull(driver.getTitle());
+        driver.quit();
+    }
+
+    @Test
+    public void testHomePageURL() {
+        WebDriver driver = getDriver();
+        driver.get("http://localhost:5173");
+        Assert.assertTrue(driver.getCurrentUrl().contains("localhost"));
+        driver.quit();
+    }
+
+    @Test
+    public void testPageSourceNotEmpty() {
+        WebDriver driver = getDriver();
+        driver.get("http://localhost:5173");
+        Assert.assertTrue(driver.getPageSource().length() > 0);
+        driver.quit();
+    }
+
+    private WebDriver getDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        return new ChromeDriver(options);
+    }
+}
